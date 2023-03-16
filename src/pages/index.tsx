67be63f949970a69c78a8971017ad6c1cdd3f4b2
Mikko Murto <mikko.murto@hhpartners.fi>
2023-03-16 14:52:58 +0200

    add pages action

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,10 +10,7 @@ import styles from './index.module.css';
 export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
-    >
+    <Layout title={`${siteConfig.title}`} description="OSS Review Toolkit">
       <main>
         <HomepageFeatures />
       </main>
